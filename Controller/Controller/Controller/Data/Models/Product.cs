5cafad042242a9702494ba142a8154e14e6a8137
JoaoBernard0 <126477093+JoaoBernard0@users.noreply.github.com>
2025-10-24 17:23:59 -0300

    Create Product.cs

---
+++ Controller/Controller/Controller/Data/Models/Product.cs
@@ -0,0 +1,10 @@
+namespace DatabaseEstoque.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+        public int Quantidade { get; set; }
+    }
+}
