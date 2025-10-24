5f9a0206d81cd4ce67d2e2beb556538faf99d09d
JoaoBernard0 <126477093+JoaoBernard0@users.noreply.github.com>
2025-10-24 17:34:32 -0300

    Create AppDbContext.cs

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using DatabaseEstoque.Models;
+
+namespace DatabaseEstoque.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
